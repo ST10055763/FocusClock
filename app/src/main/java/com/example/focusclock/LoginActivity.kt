fb7bce9a6bd05af92b78bcb848e25c507da83055
Kiashen Maharajh <ST10055763@vcconnect.edu.za>
2024-04-23 08:15:32 +0200

    updated design

--- app/src/main/java/com/example/focusclock/LoginActivity.kt
+++ app/src/main/java/com/example/focusclock/LoginActivity.kt
@@ -1,12 +1,18 @@
 package com.example.focusclock
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var  forgotPasswordLabel: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +21,13 @@ class LoginActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        forgotPasswordLabel = findViewById(R.id.tvLogForgotPass)
+
+        forgotPasswordLabel.setOnClickListener{
+            var forgotPassIntent = Intent(this, ForgotPasswordActivity::class.java)
+            startActivity(forgotPassIntent)
         }
     }
 }
