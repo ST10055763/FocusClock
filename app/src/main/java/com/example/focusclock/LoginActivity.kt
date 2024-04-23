ab83227f98b79781c91fd674da51f1bf6e36aa05
Kiashen Maharajh <ST10055763@vcconnect.edu.za>
2024-04-23 17:09:46 +0200

    added all login functionality to application

--- app/src/main/java/com/example/focusclock/LoginActivity.kt
+++ app/src/main/java/com/example/focusclock/LoginActivity.kt
@@ -5,15 +5,26 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 
+    // declare form components
     private lateinit var  forgotPasswordLabel: TextView
     private lateinit var signUpLabel: TextView
+    private lateinit var etEmail: EditText
+    private lateinit var etPass: EditText
+    private lateinit var btnLogin: Button
+
+    // Creating firebaseAuth object
+    lateinit var auth: FirebaseAuth
+
+    // main method
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -24,17 +35,52 @@ class LoginActivity : AppCompatActivity() {
             insets
         }
 
+        // assign form components to backend
         forgotPasswordLabel = findViewById(R.id.tvLogForgotPass)
         signUpLabel = findViewById(R.id.tvLogSignUp)
+        etEmail = findViewById(R.id.edtLogEmail)
+        etPass = findViewById(R.id.edtLogPass)
+        btnLogin = findViewById(R.id.btnLogLogin)
 
+        // initialising Firebase auth object
+        auth = FirebaseAuth.getInstance()
+
+        btnLogin.setOnClickListener {
+            // call login method
+            login()
+        }
+
+        // set listener for Forgot Password
         forgotPasswordLabel.setOnClickListener{
             var forgotPassIntent = Intent(this, ForgotPasswordActivity::class.java)
             startActivity(forgotPassIntent)
+            // using finish() to end the activity
+            finish()
         }
 
+        // set listener for Sign Up
         signUpLabel.setOnClickListener{
             var signUpIntent = Intent(this, SignInActivity::class.java)
             startActivity(signUpIntent)
+            // using finish() to end the activity
+            finish()
+        }
+    }
+
+    private fun login() {
+
+        // retrieve login details
+        val email = etEmail.text.toString()
+        val pass = etPass.text.toString()
+
+        // calling signInWithEmailAndPassword(email, pass)
+        // function using Firebase auth object
+        // On successful response Display a Toast
+        auth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(this) {
+            if (it.isSuccessful) {
+                Toast.makeText(this, "Successfully Logged In", Toast.LENGTH_SHORT).show()
+            } else
+                Toast.makeText(this, "Log In failed ", Toast.LENGTH_SHORT).show()
         }
     }
 }
