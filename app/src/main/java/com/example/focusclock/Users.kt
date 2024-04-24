a3519d45c7c5cd072c46870ba452b659141e34fc
Kiashen Maharajh <ST10055763@vcconnect.edu.za>
2024-04-24 19:27:20 +0200

    reworked and fixed all signup code and updated the user model

--- app/src/main/java/com/example/focusclock/Users.kt
+++ app/src/main/java/com/example/focusclock/Users.kt
@@ -1,7 +1,10 @@
 package com.example.focusclock
 
+// updated user model
 data class Users (
+    val firebaseUUID: String,
     val fname: String,
+    val email: String,
     val phoneNum : String,
     val mingoals : String,
     val maxgoals : String
