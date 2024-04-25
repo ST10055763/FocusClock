e311eaf6604d14a232946dc0108e62145d702d73
Kiashen Maharajh <ST10055763@vcconnect.edu.za>
2024-04-25 20:41:14 +0200

    added further backend homepage coding

--- app/src/main/java/com/example/focusclock/HomePageActivity.kt
+++ app/src/main/java/com/example/focusclock/HomePageActivity.kt
@@ -2,17 +2,23 @@ package com.example.focusclock
 
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.firestore.FirebaseFirestore
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Locale
 
 class HomePageActivity : AppCompatActivity() {
 
+    // Define your Firestore instance
+    private val db = FirebaseFirestore.getInstance()
+
     private lateinit var tvDateHeader : TextView
+    private lateinit var tvUserHeader: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -31,6 +37,36 @@ class HomePageActivity : AppCompatActivity() {
 
         tvDateHeader = findViewById(R.id.tvHomePageDateHeader)
         tvDateHeader.setText("Here's Todays Schedule " + currentDate)
+
+        tvUserHeader = findViewById(R.id.tvHomePageUHeader)
+
+        // Retrieve the document from Firestore based on userId
+        if (firebaseUUID != null) {
+            db.collection("profiles")
+                .document(firebaseUUID)
+                .get()
+                .addOnSuccessListener { document ->
+                    if (document.exists()) {
+                        // NOTE: CAN ALSO RETRIEVE MIN AND MAX HOURS HERE
+                        // Document exists, retrieve the "fname" field
+                        val fname = document.getString("fname")
+                        if (fname != null) {
+                            // fname is not null, you can use it
+                            tvUserHeader.setText("Hello, " + fname)
+                        } else {
+                            // fname is null, handle appropriately
+                            Toast.makeText(this, "Failed to retrieve first name", Toast.LENGTH_SHORT).show()
+                        }
+                    } else {
+                        // Document does not exist
+                        Toast.makeText(this, "User profile does not exist", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                .addOnFailureListener { e ->
+                    // Handle failures
+                    Toast.makeText(this, "Failed to retrieve user profile: ${e.message}", Toast.LENGTH_SHORT).show()
+                }
+        }
 
     }
 
