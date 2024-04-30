80236dc7fd8fead12e9f0bd0c70b1476ca5fd090
ST10095103 <ST10095103@vcconnect.edu.za>
2024-04-30 12:30:57 +0200

    Added code for AddProject button

--- app/src/main/java/com/example/focusclock/ViewProjectsActivity.kt
+++ app/src/main/java/com/example/focusclock/ViewProjectsActivity.kt
@@ -1,12 +1,15 @@
 package com.example.focusclock
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ViewProjectsActivity : AppCompatActivity() {
+    lateinit var addProject : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +18,13 @@ class ViewProjectsActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        addProject = findViewById(R.id.floatActButVPAddProj)
+        addProject.setOnClickListener{
+            var returnLoginIntent = Intent(this, AddAProjectActivity::class.java)
+            startActivity(returnLoginIntent)
+            // using finish() to end the activity
+            finish()
         }
     }
 }
