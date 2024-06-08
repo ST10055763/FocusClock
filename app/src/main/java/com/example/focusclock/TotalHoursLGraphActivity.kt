e232339724ab9c26d852358d1bd3b623ef196f79
Kiashen Maharajh <ST10055763@vcconnect.edu.za>
2024-06-08 10:56:45 +0200

    added line graph page, activity and design

---
+++ app/src/main/java/com/example/focusclock/TotalHoursLGraphActivity.kt
@@ -0,0 +1,20 @@
+package com.example.focusclock
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class TotalHoursLGraphActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_total_hours_lgraph)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
